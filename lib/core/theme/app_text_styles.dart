5ced6b8ac286f047a7a7fef24ee3dd7ab357de2b
salsabilelshiekh2022 <salsabilelshiekh6@gmail.com>
2024-04-19 13:15:42 +0200

    -Add theming classes

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mentorship/core/theme/app_colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class AppTextStyle {
+  static TextStyle font24WhiteWeight600 = TextStyle(
+    fontSize: 24.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle font18WhiteWeight600 = TextStyle(
+    fontSize: 18.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle font24WhiteWeight400 = TextStyle(
+    fontSize: 24.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle font18WhiteWeight400 = TextStyle(
+    fontSize: 18.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle font16WhiteWeight400 = TextStyle(
+    fontSize: 16.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle font16WhiteWeight600 = TextStyle(
+    fontSize: 16.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle font22WhiteWeight600 = TextStyle(
+    fontSize: 22.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle font22WhiteWeight400 = TextStyle(
+    fontSize: 22.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle font20WhiteWeight600 = TextStyle(
+    fontSize: 20.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle font20WhiteWeight400 = TextStyle(
+    fontSize: 20.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle font14WhiteWeight600 = TextStyle(
+    fontSize: 14.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle font14WhiteWeight400 = TextStyle(
+    fontSize: 14.sp,
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+  );
+}
